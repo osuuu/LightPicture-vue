b2296683329767d6476ea6f52470eb7ee7a81b35
初涧 <admin@osuu.net>
2022-01-16 16:01:08 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,7 @@ module.exports = {
     publicPath: '/',
     outputDir: 'dist',
     assetsDir: 'static',
-    // publicPath: "/u/",
-    // pages: {
-
-    //     admin: 'src/pages/admin/main.js',
-    //     index: 'src/pages/index/main.js',
-    //     // client: 'src/modules/client/client.js'
-    //     // ,index:"src/main.js"
-    // }
-
+  
 
 
 }
